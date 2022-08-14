b5f06300400e754e92e87c5623901e2ad2839f3a
Tachytaenius <henryfthomson@gmail.com>
2022-08-14 10:53:51 +0100

    Move tiles from src/res/ to src/res/tiles/

--- src/tileset.asm
+++ src/tileset.asm
@@ -3,20 +3,20 @@ INCLUDE "include/constants.inc"
 MACRO define_tile
 	DEF \1 RB
 	EXPORT \1
-	INCBIN \2
+	INCBIN STRCAT("res/tiles/", \2, ".2bpp")
 ENDM
 
 SECTION "Tileset graphics", ROM0
 
 TilesetGraphics::
 	RSRESET
-	define_tile TILE_EMPTY, "res/empty.2bpp"
-	define_tile TILE_WALL, "res/wall.2bpp"
-	define_tile TILE_PLAYER, "res/player.2bpp"
-	define_tile TILE_FLOOR, "res/floor.2bpp"
-	define_tile TILE_EMPTY_BLACK, "res/empty_black.2bpp"
-	define_tile TILE_DOOR, "res/door.2bpp"
-	define_tile TILE_ICE, "res/ice.2bpp"
+	define_tile TILE_EMPTY, "empty"
+	define_tile TILE_WALL, "wall"
+	define_tile TILE_PLAYER, "player"
+	define_tile TILE_FLOOR, "floor"
+	define_tile TILE_EMPTY_BLACK, "empty_black"
+	define_tile TILE_DOOR, "door"
+	define_tile TILE_ICE, "ice"
 .end::
 
 DEF NUM_TILES RB 0
