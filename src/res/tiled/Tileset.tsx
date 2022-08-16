33e41cb3d5896824d5dc5744927fee8c95683b57
Tachytaenius <henryfthomson@gmail.com>
2022-08-16 20:40:10 +0100

    Update Tileset.tsx with new tile locations

--- src/res/tiled/Tileset.tsx
+++ src/res/tiled/Tileset.tsx
@@ -2,24 +2,24 @@
 <tileset version="1.9" tiledversion="1.9.0" name="Tileset" tilewidth="8" tileheight="8" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="8" height="8" source="../empty.png"/>
+  <image width="8" height="8" source="../tiles/empty.png"/>
  </tile>
  <tile id="1">
-  <image width="8" height="8" source="../wall.png"/>
+  <image width="8" height="8" source="../tiles/wall.png"/>
  </tile>
  <tile id="2">
-  <image width="8" height="8" source="../player.png"/>
+  <image width="8" height="8" source="../tiles/player.png"/>
  </tile>
  <tile id="3">
-  <image width="8" height="8" source="../floor.png"/>
+  <image width="8" height="8" source="../tiles/floor.png"/>
  </tile>
  <tile id="4">
-  <image width="8" height="8" source="../empty_black.png"/>
+  <image width="8" height="8" source="../tiles/empty_black.png"/>
  </tile>
  <tile id="5">
-  <image width="8" height="8" source="../door.png"/>
+  <image width="8" height="8" source="../tiles/door.png"/>
  </tile>
  <tile id="6">
-  <image width="8" height="8" source="../ice.png"/>
+  <image width="8" height="8" source="../tiles/ice.png"/>
  </tile>
 </tileset>
